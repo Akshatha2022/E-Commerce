b7c9439c0db9fa610b1827686be1e30a9661fcae
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-02-22 20:33:43 -0600

    define column

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,22 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // defining a product_id column
+    product_id: {
+      // it uses the special Sequelize DataTypes object which provides what type of data it is
+      type: DataTypes.INTEGER,
+      //it is equivalent of SQL's `NOT NULL` option
+      allowNull: false,
+      // instructing that this is the Primary Key
+      primaryKey: true,
+      // turn on auto increment
+      autoIncrement: true
+  },
+  // defining a tag_id column
+  tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+  }
   },
   {
     sequelize,
