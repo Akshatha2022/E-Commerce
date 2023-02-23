98c2eb7693689ddd8c2a759c40e1e7e1495cee7e
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-02-22 21:29:05 -0600

    still adding reference to models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -16,11 +16,19 @@ ProductTag.init(
     // defining a product_id column
     product_id: {
       // it uses the special Sequelize DataTypes object which provides what type of data it is
-      type: DataTypes.INTEGER
+      type: DataTypes.INTEGER,
+      references: {
+          model: "product",
+          key: "id"
+      }
   },
   // defining a tag_id column
   tag_id: {
-    type: DataTypes.INTEGER
+    type: DataTypes.INTEGER,
+    references: {
+        model: "tag",
+        key: "id"
+    }
   }
   },
   {
