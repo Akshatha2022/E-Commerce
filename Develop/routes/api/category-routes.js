b9b30c158fb958728376fc38f2a9db91c569f574
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-02-22 21:35:39 -0600

    adding associated methods to the sequilize models

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -7,7 +7,15 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
 
-  Category.findAll()
+  Category.findAll({
+    attributes: ['id', 'category_name'],
+    include: [
+        {
+            model: Product,
+            attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+    ]
+})
         .then(dbCategoryData => res.json(dbCategoryData))
         .catch(err => {
             console.log(err);
@@ -22,7 +30,14 @@ router.get('/:id', (req, res) => {
   Category.findOne({
     where: {
         id: req.params.id
-    }
+    },
+    attributes: ['id', 'category_name'],
+    include: [
+        {
+            model: Product,
+            attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+    ]
 })
     .then(dbCategoryData => {
         if (!dbCategoryData) {
